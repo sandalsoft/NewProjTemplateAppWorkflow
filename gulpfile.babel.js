efa2537096538cf6e85ff3f775396ac03053f5d1
Eric Nelson <eric@sandalsoft.com>
2018-03-01 00:09:15 -0600

    WORKING imports, working gulp.babel.js, working index transpile with spread.  But gulpfile.babel.js gives warning about register module

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,30 @@
-// import gulp from 'gulp';
-// import axios from 'axios';
-// import Directory from 'directory-helpers';
+import gulp from 'gulp';
+import axios from 'axios';
+import Directory from 'directory-helpers';
 
-const gulp = require('gulp');
-const axios = require('axios');
+// const gulp = require('gulp');
+// const axios = require('axios');
 
 const repoURL =
   'https://raw.githubusercontent.com/sandalsoft/NewProjTemplateAppWorkflow/master/';
 
-// const fetchContent = url => axios(url);
+const fetchContent = url => axios(url);
 
-// const getEslint = async url => await fetchContent(`${url}.eslintrc`);
+const getEslint = async url => await fetchContent(`${url}.eslintrc`);
 
-// const writeFile = async (filepath, content) => {};
+const writeFile = async (filepath, content) => {};
 // create a default task
 gulp.task('default', ['eslint']);
 
-gulp.task('eslint', function() {
-  console.log('tich');
+gulp.task('eslint', async function() {
+  try {
+    const eslint = await getEslint(repoURL);
+    console.log(eslint.data);
+  } catch (e) {
+    throw e;
+  }
 });
-// gulp.task('eslint', async function() {
-//   try {
-//     // const eslint = await getEslint(repoURL);
-//     console.log('h');
-//   } catch (e) {
-//     throw e;
-//   }
+
+// gulp.task('eslint', function() {
+//   console.log('tich');
 // });
