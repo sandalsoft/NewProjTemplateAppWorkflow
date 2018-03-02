c76cedd3bc95ab86ac596fa8ec2edf78353e3f87
Eric Nelson <eric@sandalsoft.com>
2018-03-02 15:40:32 -0600

    Added smarter gists for git work

--- runner.bash
+++ runner.bash
@@ -1,23 +1,28 @@
-#!/usr/bin/env bash
-
-# Save the script arguments
-SCRIPT_NAME=$0
-APP_TYPE=$1 #node or react
-APP_NAME=$2 # Name of app
+#!/bin/bash
 
 cd output;
 
-git init;
+CURRENTDIR=${PWD##*/}
+GITHUBUSER=$(git config github.user)
+GITHUB_TOKEN=$(git config github.token)
 
-git remote add origin https://github.com/sandalsoft/bashme
 
-git pull origin master
+# Get user input
+read -p "New repo name (enter for ${PWD##*/}):" REPONAME
+read -p "Git Username (enter for ${GITHUBUSER}):" USER
+read -p "Repo Description:" DESCRIPTION
 
-touch popo.txt
 
-git add .
 
-git commit -m "INITIAL COMMIT"
+echo "Creating ${USER:-${GITHUBUSER}}/${REPONAME:-${CURRENTDIR}} on Github.com"
 
-git push origin master
+curl -u $GITHUB_TOKEN:x-oauth-basic https://api.github.com/user/repos -d "{\"name\": \"${REPONAME:-${CURRENTDIR}}\", \"description\": \"${DESCRIPTION}\", \"private\": true, \"has_issues\": true, \"has_downloads\": true, \"has_wiki\": true}"
+
+# Set the freshly created repo to the origin and push
+# You'll need to have added your public key to your github account
+echo "Setting remote origin for ${USER:-${GITHUBUSER}}/${REPONAME:-${CURRENTDIR}}"
+git remote set-url origin git@github.com:${USER:-${GITHUBUSER}}/${REPONAME:-${CURRENTDIR}}.git
+
+echo "Pushing new repo to Github"
+git push --set-upstream origin master
 
