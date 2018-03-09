99d9dc7ee76dc535c4896398106b146c1eeb5874
Eric Nelson <eric@sandalsoft.com>
2018-03-09 12:57:19 -0600

    working on minimist plus prompts interactive and cmdline arg hybrid

--- src/git/init-git-repo.js
+++ src/git/init-git-repo.js
@@ -1,3 +1,18 @@
+import fs from 'fs-extra';
+import path from 'path';
+
 import { exec } from '../util';
 
-export const initGitRepo = async () => await exec('git init');
+export const initGitRepo = async (repoPath = path.basename(process.cwd())) => {
+  const gitPath = path.join(repoPath, '.git');
+
+  try {
+    if (fs.statSync(gitPath).isDirectory()) {
+      return Promise.reject(new Error('Git repo already exists'));
+    } else {
+      await exec('git init');
+    }
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
