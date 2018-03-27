584d42f4f0137225046eefc23d226fcf856bd458
Eric Nelson <eric@sandalsoft.com>
2018-03-27 16:10:15 -0500

    jest working with es6 imports.  Not sure its working with .ts files

--- src/util/input-is-valid.test.ts
+++ src/util/input-is-valid.test.ts
@@ -1,13 +1,13 @@
 import { inputIsValid } from './input-is-valid';
 
 test('/etc/passwd is invalid', () => {
-  const expected = 'false';
+  const expected = false;
   const actual = inputIsValid('/etc/passwd');
   expect(actual).toEqual(expected);
 });
 
 test('../whatever is invalid', () => {
   const actual = true;
-  const expected = inputIsValid('../whatever');
+  const expected = inputIsValid('../whateverXX');
   expect(actual).toEqual(expected);
 });
